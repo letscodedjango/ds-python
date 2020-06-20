c26bc626dd43c63355999757ae3541ee94ebcece
Rajeshwar Sharma <rajeshwar.sharma@cerence.com>
2020-06-20 23:14:17 +0530

    Added project template for data science project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a data science project to predict the survival of people from titanic disaster',
+    author='rsharma',
+    license='',
+)
